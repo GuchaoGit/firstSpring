0cd2b365501c2023c64a56302cadc9ec55016135
guchaochao <guchaochao@huiyunit.com>
2020-01-06 14:44:12 +0800

    松耦合实现升级

--- src/main/java/com/guc/fristspring/App.java
+++ src/main/java/com/guc/fristspring/App.java
@@ -14,7 +14,7 @@ public class App {
     private static ApplicationContext context;
 
     public static void main(String[] args) {
-        context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        context = new ClassPathXmlApplicationContext("SpringBeans.xml","Spring-Output.xml");
         Hello hello = (Hello) context.getBean("helloBean");
         hello.printHello();
         Person person = (Person) context.getBean("personBean");
